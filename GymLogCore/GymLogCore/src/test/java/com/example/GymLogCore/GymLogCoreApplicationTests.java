cb4074b3ba5bcdb8966ae78bb7c63eff3cfda1b2
sqtwix <ivan20140767@gmail.com>
2026-04-25 11:40:35 +0300

    feat: added Mobile App and Java Backend project

---
+++ GymLogCore/GymLogCore/src/test/java/com/example/GymLogCore/GymLogCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GymLogCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymLogCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
